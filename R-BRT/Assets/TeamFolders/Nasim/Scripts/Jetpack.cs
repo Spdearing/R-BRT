1b02d4f097c750b20a0ff1b3f21e292074c3ef55
nasimi10 <loveschnii@gmail.com>
2024-06-04 20:18:42 -0400

    Battery Script Made

--- R-BRT/Assets/TeamFolders/Nasim/Scripts/Jetpack.cs
+++ R-BRT/Assets/TeamFolders/Nasim/Scripts/Jetpack.cs
@@ -3,79 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jetpack : MonoBehaviour
-{
-    public bool canUseJetpack;
-    public float jetpackAcceleration = 25f;
-    
-    [Range(0f, 1f)]
-    public float jetpackDownwardVelocityCanceling = 0.75f;
-
-    public float consumeDuration = 1.5f;
-    public float refuelDurationGrounded = 2f;
-    public float refuelDurationFlying = 5f;
-    public float refuelDelay = 1f;
-
-    public float currentFuelRatio;
-    float lastTimeofUse;
-
-    PlayerController playerCharacterController;
-    [SerializeField] private TrailRenderer tr;
-
-
-    void Start()
     {
-        playerCharacterController = GetComponent<PlayerController>();
+        public bool canUseJetpack;
+        public float jetpackAcceleration = 25f;
 
-        currentFuelRatio = 1f;
-        tr.emitting = false;
+        [Range(0f, 1f)]
+        public float jetpackDownwardVelocity = 0.75f;
 
-    }
+        public float consumeFuelDuration = 1.5f;
+        public float refuelDurationGrounded = 2f;
+        public float refuelDurationAirborne = 5f;
+        public float refuelDelay = 1f;
 
+        public float currentFillRatio;
+        float lastTimeOfUse;
 
-    void Update()
-    {
-        if (playerCharacterController.isGrounded)
+        PlayerController playerCharacterController;
+        [SerializeField] private TrailRenderer tr;
+
+        void Start()
         {
-            canUseJetpack = false;
-        }
-        else if (!playerCharacterController.hasJumpedThisFrame && Input.GetKeyDown(KeyCode.Space))
-        {
-            canUseJetpack = true;
-        }
+            playerCharacterController = GetComponent<PlayerController>();
 
-        bool jetpackIsInUse = canUseJetpack && currentFuelRatio > 0f && Input.GetKey(KeyCode.Space);
-
-        if(jetpackIsInUse)
-        {
-            tr.emitting = true;
-
-            lastTimeofUse = Time.time;
-
-            float totalAcceleration = jetpackAcceleration;
-
-            totalAcceleration += playerCharacterController.gravity;
-
-            if (playerCharacterController.velocity.y < 0f)
-            {
-                totalAcceleration += ((-playerCharacterController.velocity.y / Time.deltaTime) * jetpackDownwardVelocityCanceling);
-            }
-
-            playerCharacterController.velocity += Vector3.up * totalAcceleration * Time.deltaTime;
-
-            currentFuelRatio = currentFuelRatio - (Time.deltaTime / consumeDuration);
-        }
-
-        else 
-        {
+            currentFillRatio = 1f;
             tr.emitting = false;
+        }
 
-            if (Time.time - lastTimeofUse >= refuelDelay)
-            {
-                float refuelRate = 1 / (playerCharacterController.isGrounded ? refuelDurationGrounded : refuelDurationFlying);
-                currentFuelRatio = currentFuelRatio + Time.deltaTime * refuelRate;
-
+        void Update()
+        {
+            if (playerCharacterController.isGrounded) {
+                canUseJetpack = false;
+            } else if (!playerCharacterController.hasJumpedThisFrame && Input.GetKeyDown(KeyCode.Space)) {
+                canUseJetpack = true;
             }
-            currentFuelRatio = Mathf.Clamp01(currentFuelRatio);
+
+
+            bool jetpackIsInUse = canUseJetpack && currentFillRatio > 0f && Input.GetKey(KeyCode.Space);
+
+            if(jetpackIsInUse) {
+
+                tr.emitting = true;
+
+                lastTimeOfUse = Time.time;
+
+                float totalAcceleration = jetpackAcceleration;
+
+                totalAcceleration += playerCharacterController.gravity;
+
+                if (playerCharacterController.velocity.y < 0f) {
+
+                    totalAcceleration += ((-playerCharacterController.velocity.y / Time.deltaTime) * jetpackDownwardVelocity);
+                }
+
+                playerCharacterController.velocity += Vector3.up * totalAcceleration * Time.deltaTime;
+
+                currentFillRatio = currentFillRatio - (Time.deltaTime / consumeFuelDuration);
+
+            } else {
+                tr.emitting = false;
+
+                if (Time.time - lastTimeOfUse >= refuelDelay)
+                {
+                    float refillRate = 1 / (playerCharacterController.isGrounded ? refuelDurationGrounded : refuelDurationAirborne);
+                    currentFillRatio = currentFillRatio + Time.deltaTime * refillRate;
+
+                }
+                currentFillRatio = Mathf.Clamp01(currentFillRatio);
+            }
+
+             
+
         }
     }
-}
