7b8b4d7a2128f3dc72ab04e78c00c98ed86efab7
nasimi10 <loveschnii@gmail.com>
2024-06-07 21:54:43 -0400

    canvas enabled

--- R-BRT/Assets/TeamFolders/Nasim/Scripts/Battery.cs
+++ R-BRT/Assets/TeamFolders/Nasim/Scripts/Battery.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Battery : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class Battery : MonoBehaviour
     public GameObject player;
     Jetpack jetPack;
 
+    public Canvas GUICanvas;
+    public GameObject interactableText;
+
     // Start is called before the first frame update
     void Start()
     {
+        GUICanvas.gameObject.SetActive(false);
+        interactableText.gameObject.SetActive(false);
+
         rb = GetComponent<Rigidbody>();
         gameObject.tag = "Battery";
         jetPack = player.GetComponent<Jetpack>();
@@ -24,7 +31,12 @@ public class Battery : MonoBehaviour
 
     private void OnTriggerEnter(Collider col)
     {
-        jetPack.enabled = true;
-        Destroy(gameObject);
+        if (col.gameObject.CompareTag("Player"))
+        {
+            interactableText.gameObject.SetActive(true);
+            GUICanvas.gameObject.SetActive(true);
+        }
+        /*jetPack.enabled = true;
+        Destroy(gameObject);*/
     }
 }
