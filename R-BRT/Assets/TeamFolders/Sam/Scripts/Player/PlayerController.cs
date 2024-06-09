37b6edb6d6a587889bc8a073786d265cb1ab9260
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-06-09 15:58:03 -0400

    changed 1.5 to 2

--- R-BRT/Assets/TeamFolders/Sam/Scripts/Player/PlayerController.cs
+++ R-BRT/Assets/TeamFolders/Sam/Scripts/Player/PlayerController.cs
@@ -329,7 +329,7 @@ public class PlayerController : MonoBehaviour
 
     private bool OnSlope()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 1.5f))
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 2.0f))
         {
             float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
             return angle < maxSlopeAngle && angle != 0;
