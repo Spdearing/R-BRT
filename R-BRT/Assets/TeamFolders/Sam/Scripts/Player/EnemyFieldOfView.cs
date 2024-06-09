817a2c30bec6e46ff7741fd287dfea4da2b51d8e
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-06-09 13:17:19 -0400

    State machine for the Ground Robot is almost done

--- R-BRT/Assets/TeamFolders/Sam/Scripts/Player/EnemyFieldOfView.cs
+++ R-BRT/Assets/TeamFolders/Sam/Scripts/Player/EnemyFieldOfView.cs
@@ -4,25 +4,21 @@ using UnityEngine;
 
 public class EnemyFieldOfView : MonoBehaviour
 {
+    [Header("Bools")]
     [SerializeField] bool playerSpotted;
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("Seeing the Player");
-            Debug.Log(playerSpotted);
             playerSpotted = true;
-            Debug.Log(playerSpotted);
         }
-        
     }
 
     public void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Player went away");
             playerSpotted = false;
         }
     }
