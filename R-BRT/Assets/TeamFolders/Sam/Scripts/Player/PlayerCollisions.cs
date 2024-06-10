340f89e47d296751ec4ec8d90eb28606d9e02a09
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-06-10 19:38:41 -0400

    player has collisions script on it

---
+++ R-BRT/Assets/TeamFolders/Sam/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    [SerializeField] GameObject destinationA;
+    [SerializeField] GameObject destinationB;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Death")
+        {
+            transform.position = destinationA.transform.position;
+        }
+    }
+}
