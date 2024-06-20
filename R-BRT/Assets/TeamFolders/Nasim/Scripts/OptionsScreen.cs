5564aed8ba7b84d8ec87dbde643ef4a48361c94e
nasimi10 <loveschnii@gmail.com>
2024-06-20 18:06:09 -0400

    Player can now change Resolution

--- R-BRT/Assets/TeamFolders/Nasim/Scripts/OptionsScreen.cs
+++ R-BRT/Assets/TeamFolders/Nasim/Scripts/OptionsScreen.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsScreen : MonoBehaviour
 {
     public Toggle fullscreenToggle, vsyncToggle;
-    // Start is called before the first frame update
+
+    public List<ResolutionItem> resolutions = new List<ResolutionItem>();
+    private int selectedResolution;
+    
+    public TMP_Text resolutionLabel;
+
     void Start()
     {
         fullscreenToggle.isOn = Screen.fullScreen;
@@ -19,17 +25,72 @@ public class OptionsScreen : MonoBehaviour
         {
             vsyncToggle.isOn = true;
         }
+
+        bool foundResolution = false;
+        for(int i = 0; i < resolutions.Count; i++)
+        {
+            if(Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundResolution = true;
+
+                selectedResolution = i;
+
+                UpdateResolutionLabel();
+            }
+        }
+
+        if(!foundResolution)
+        {
+            ResolutionItem newResolution = new ResolutionItem();
+            newResolution.horizontal = Screen.width;
+            newResolution.vertical = Screen.height;
+
+            resolutions.Add(newResolution);
+            selectedResolution = resolutions.Count - 1;
+
+            UpdateResolutionLabel();
+        }
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
 
+    public void ResolutionLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+
+        UpdateResolutionLabel();
+
+    }
+
+    public void ResolutionRight()
+    {
+        selectedResolution++;
+        if(selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResolutionLabel();
+
+    }
+
+    public void UpdateResolutionLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
     public void ApplyGraphicsChanges()
     {
-        Screen.fullScreen = fullscreenToggle.isOn;
+        //Screen.fullScreen = fullscreenToggle.isOn;
 
         if(vsyncToggle.isOn)
         {
@@ -39,6 +100,16 @@ public class OptionsScreen : MonoBehaviour
         {
             QualitySettings.vSyncCount = 0;
         }
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenToggle.isOn);
     }
+}
+
+[System.Serializable]
+public class ResolutionItem
+{
+    public int horizontal, vertical;
+
+
 
 }
