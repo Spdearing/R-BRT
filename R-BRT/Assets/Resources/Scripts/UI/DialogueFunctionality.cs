9656230f5101e9b3713adf6a48d90a1f9b810ef0
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-06-22 11:09:53 -0400

    Dialogue basic functionality created

---
+++ R-BRT/Assets/Resources/Scripts/UI/DialogueFunctionality.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueFunctionality : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private string fullText;
+    [SerializeField] private float delay;
+
+    [SerializeField] private string currentText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        delay = .035f;
+        StartCoroutine(ShowDialogue());
+    }
+
+    IEnumerator ShowDialogue()
+    {
+        for(int i = 0; i <= fullText.Length; i++) 
+        {
+            currentText = fullText.Substring(0, i);
+            uiText.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
