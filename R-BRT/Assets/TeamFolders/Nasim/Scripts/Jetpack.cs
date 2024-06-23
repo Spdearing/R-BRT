3a552f728571744cc040bbebaf61927091f64e09
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-06-23 17:08:31 -0400

    Jet pack is fixed, does not activate on hold immediatyl

--- R-BRT/Assets/TeamFolders/Nasim/Scripts/Jetpack.cs
+++ R-BRT/Assets/TeamFolders/Nasim/Scripts/Jetpack.cs
@@ -4,113 +4,121 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Jetpack : MonoBehaviour
+{
+    [Header("Bools")]
+    [SerializeField] private bool pressedFirstTime;
+    [SerializeField] private bool canUseJetpack;
+
+    [Header("Floats")]
+    [SerializeField] private float jetpackAcceleration = 15f;
+    [SerializeField] private float delayBetweenPress = 0.25f;
+    [SerializeField] private float lastPressedTime;
+    [SerializeField] private float jetpackDownwardVelocity = 0.75f;
+    [SerializeField] private float consumeFuelDuration = 1.5f;
+    [SerializeField] private float refuelDurationGrounded = 2f;
+    [SerializeField] private float refuelDurationAirborne = 3f;
+    [SerializeField] private float refuelDelay = 1f;
+    [SerializeField] private float currentFillRatio;
+    [SerializeField] private float lastTimeOfUse;
+
+    [Range(0f, 1f)]
+
+
+    [Header("Scripts")]
+    [SerializeField] private PlayerController playerCharacterController;
+
+    [Header("Trail Renderer")]
+    [SerializeField] private TrailRenderer tr;
+
+    [Header("Slider")]
+    [SerializeField] private Slider fuelMeter;
+
+    void Start()
     {
-        public bool canUseJetpack;
-        public float jetpackAcceleration = 15f;
 
-        public float delayBetweenPress = 0.25f;
-        public bool pressedFirstTime = false;
-        public float lastPressedTime;
+        pressedFirstTime = false;
+        playerCharacterController = GetComponent<PlayerController>();
+        fuelMeter = GameObject.FindWithTag("Canvas").GetComponentInChildren<Slider>();
+        tr = GetComponent<TrailRenderer>();
 
-        [Range(0f, 1f)]
-        public float jetpackDownwardVelocity = 0.75f;
-
-        public float consumeFuelDuration = 1.5f;
-        public float refuelDurationGrounded = 2f;
-        public float refuelDurationAirborne = 3f;
-        public float refuelDelay = 1f;
-
-        public float currentFillRatio;
-        float lastTimeOfUse;
-
-        PlayerController playerCharacterController;
-        [SerializeField] private TrailRenderer tr;
-
-        [SerializeField]
-        private Slider fuelMeter;
-
-        void Start()
-        {
-            playerCharacterController = GetComponent<PlayerController>();
-            tr = GetComponent<TrailRenderer>();
-
-            currentFillRatio = 1f;
-            tr.emitting = false;
-        }
-
-        void Update()
-        {
-            if (playerCharacterController.ReturnIsGrounded()) 
-            {
-                canUseJetpack = false;
-            } 
-            else if (Input.GetKey(KeyCode.Space)) 
-            {
-                canUseJetpack = true;
-            }
-
-            if(Input.GetKeyDown(KeyCode.Space))
-            {
-                if(pressedFirstTime)
-                {
-                    bool isDoubledPress = Time.time -lastPressedTime <= delayBetweenPress;
-
-                    if(isDoubledPress)
-                    {
-                        Debug.Log("See if Jetpack Activated");
-                        pressedFirstTime = false;
-                    }
-                }
-
-                else
-                {
-                    pressedFirstTime = true;
-                }
-
-                lastPressedTime = Time.time;
-            }
-
-            if(pressedFirstTime && Time.time - lastPressedTime > delayBetweenPress)
-            {
-                Debug.Log("Jetpack On");
-                pressedFirstTime = false;
-            }
-
-
-            bool jetpackIsInUse = canUseJetpack && currentFillRatio > 0f && Input.GetKey(KeyCode.Space);
-
-            if(jetpackIsInUse) {
-
-                tr.emitting = true;
-
-                lastTimeOfUse = Time.time;
-
-                float totalAcceleration = jetpackAcceleration;
-
-                totalAcceleration += playerCharacterController.ReturnGravity();
-
-                if (playerCharacterController.rb.velocity.y < 0f) {
-
-                    totalAcceleration += ((-playerCharacterController.rb.velocity.y / Time.deltaTime) * jetpackDownwardVelocity);
-                }
-
-                playerCharacterController.rb.velocity += Vector3.up * totalAcceleration * Time.deltaTime;
-
-                currentFillRatio = currentFillRatio - (Time.deltaTime / consumeFuelDuration);
-
-            } else {
-                tr.emitting = false;
-
-                if (Time.time - lastTimeOfUse >= refuelDelay)
-                {
-                    float refillRate = 1 / (playerCharacterController.ReturnIsGrounded() ? refuelDurationGrounded : refuelDurationAirborne);
-                    currentFillRatio = currentFillRatio + Time.deltaTime * refillRate;
-
-                }
-                currentFillRatio = Mathf.Clamp01(currentFillRatio);
-            }
-
-            fuelMeter.value = currentFillRatio;
-
-        }
+        currentFillRatio = 1f;
+        tr.emitting = false;
     }
+
+    void Update()
+    {
+        if (playerCharacterController.ReturnIsGrounded())
+        {
+            canUseJetpack = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canUseJetpack = true;
+            pressedFirstTime = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (pressedFirstTime)
+            {
+                bool isDoubledPress = Time.time - lastPressedTime <= delayBetweenPress;
+
+                if (isDoubledPress)
+                {
+                    Debug.Log("See if Jetpack Activated");
+                    pressedFirstTime = false;
+                }
+            }
+
+            lastPressedTime = Time.time;
+        }
+
+        if (pressedFirstTime && Time.time - lastPressedTime > delayBetweenPress)
+        {
+            Debug.Log("Jetpack On");
+            pressedFirstTime = false;
+        }
+
+
+        bool jetpackIsInUse = canUseJetpack && currentFillRatio > 0f && Input.GetKey(KeyCode.Space);
+
+        if (jetpackIsInUse)
+        {
+
+            tr.emitting = true;
+
+            lastTimeOfUse = Time.time;
+
+            float totalAcceleration = jetpackAcceleration;
+
+            totalAcceleration += playerCharacterController.ReturnGravity();
+
+            if (playerCharacterController.rb.velocity.y < 0f)
+            {
+
+                totalAcceleration += ((-playerCharacterController.rb.velocity.y / Time.deltaTime) * jetpackDownwardVelocity);
+            }
+
+            playerCharacterController.rb.velocity += Vector3.up * totalAcceleration * Time.deltaTime;
+
+            currentFillRatio = currentFillRatio - (Time.deltaTime / consumeFuelDuration);
+
+        }
+        else
+        {
+            tr.emitting = false;
+
+            if (Time.time - lastTimeOfUse >= refuelDelay)
+            {
+                float refillRate = 1 / (playerCharacterController.ReturnIsGrounded() ? refuelDurationGrounded : refuelDurationAirborne);
+                currentFillRatio = currentFillRatio + Time.deltaTime * refillRate;
+
+            }
+            currentFillRatio = Mathf.Clamp01(currentFillRatio);
+        }
+
+        fuelMeter.value = currentFillRatio;
+
+    }
+}
