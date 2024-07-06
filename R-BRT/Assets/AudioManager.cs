d186fc8f961fcf55b2f190955df603a88ea85fcb
nasimi10 <loveschnii@gmail.com>
2024-07-06 15:24:01 -0400

    Reworking Audio Manager

--- R-BRT/Assets/AudioManager.cs
+++ R-BRT/Assets/AudioManager.cs
@@ -5,25 +5,16 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioMixer theMixer;
+	[Header("----Audio Source ----")]
+	[SerializeField] AudioSource masterSource;
+	[SerializeField] AudioSource musicSource;
+	[SerializeField] AudioSource sfxSource;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(PlayerPrefs.HasKey("MasterVol"))
-        {
-            theMixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
-        }
 
-        if(PlayerPrefs.HasKey("MusicVol"))
-        {
-            theMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
-        }
+	[Header("----Audio Source ----")]
+	public AudioClip walk;
+	public AudioClip caught;
+	public AudioClip invisibleActive;
 
-        if(PlayerPrefs.HasKey("SFXVol"))
-        {
-            theMixer.SetFloat("SFXVol", PlayerPrefs.GetFloat("SFXVol"));
-        }
-    }
 
 }
