e4b200050be5a4f024da455eb4256a39949734cf
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-07-08 20:38:37 -0400

    placing spider bots

--- R-BRT/Assets/Resources/Scripts/EnemySpawner/SpiderBotSpawner.cs
+++ R-BRT/Assets/Resources/Scripts/EnemySpawner/SpiderBotSpawner.cs
@@ -10,18 +10,15 @@ public class SpiderBotSpawner : MonoBehaviour
     [SerializeField] GameObject enemyPrefab;
 
     [Header("EnemyGroups")]
-    [SerializeField] GameObject[] group1 = new GameObject[1];
-    [SerializeField] GameObject[] group2 = new GameObject[4];
+    [SerializeField] GameObject[] group1 = new GameObject[8];
 
     [Header("EnemyGroupLocations")]
-    [SerializeField] Transform[] enemyGroup1Locations = new Transform[1];
-    [SerializeField] Transform[] enemyGroup2Locations = new Transform[4];
+    [SerializeField] Transform[] enemyGroup1Locations = new Transform[8];
 
     [Header("References")]
     [SerializeField] SpiderBotStateMachine spiderBotStateInstance;
     [SerializeField] EnemySpiderBotFieldOfView enemySpiderBotFieldOfViewInstance;
 
-    // Start is called before the first frame update
     void Start()
     {
         enemyPrefab = Resources.Load<GameObject>("Sam's_Prefabs/CameraBotFinal");
@@ -51,31 +48,6 @@ public class SpiderBotSpawner : MonoBehaviour
             }
         }
     }
-
-    void SpawnGroup2()
-    {
-        for (int i = 0; i < group2.Length; i++)
-        {
-            if (i < enemyGroup2Locations.Length)
-            {
-
-                GameObject enemy = Instantiate(enemyPrefab, enemyGroup2Locations[i].position, enemyGroup2Locations[i].rotation);
-                group2[i] = enemy;
-                SpiderBotStateMachine stateMachine = enemy.GetComponent<SpiderBotStateMachine>();
-                EnemySpiderBotFieldOfView fieldOfView = enemy.GetComponentInChildren<EnemySpiderBotFieldOfView>();
-
-                if (stateMachine != null) spiderBotStateInstance = stateMachine;
-                if (fieldOfView != null) enemySpiderBotFieldOfViewInstance = fieldOfView;
-
-                enemy.name = "SpiderBotGroup2";
-            }
-            else
-            {
-                Debug.LogWarning("Index out of bounds for enemyGroupLocations array.");
-            }
-        }
-    }
-
     public SpiderBotStateMachine ReturnSpiderBotStateInstance()
     {
         return this.spiderBotStateInstance;
