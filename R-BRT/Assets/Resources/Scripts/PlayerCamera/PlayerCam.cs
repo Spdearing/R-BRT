22182558acdf3a8d7689ad103015b2705cd1a6b2
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-07-13 21:34:20 -0400

    camera fixed

--- R-BRT/Assets/Resources/Scripts/PlayerCamera/PlayerCam.cs
+++ R-BRT/Assets/Resources/Scripts/PlayerCamera/PlayerCam.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 
-
 public class PlayerCam : MonoBehaviour
 {
     [Header("Floats")]
-    [SerializeField] private float sensX;
-    [SerializeField] private float sensY;
-    [SerializeField] private float xRotation;
-    [SerializeField] private float yRotation;
+    [SerializeField] private float sensX = 100f;
+    [SerializeField] private float sensY = 100f;
+    private float xRotation = 0f;
+    private float yRotation = 0f;
 
     [Header("Transform")]
     [SerializeField] private Transform orientation;
@@ -15,12 +14,9 @@ public class PlayerCam : MonoBehaviour
     [Header("Scripts")]
     [SerializeField] private PlayerController playerController;
 
-    
-
     // Start is called before the first frame update
     private void Start()
     {
-
         playerController = GameManager.instance.ReturnPlayerController();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -34,15 +30,13 @@ public class PlayerCam : MonoBehaviour
             float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
             float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-
             yRotation += mouseX;
-
             xRotation -= mouseY;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            // rotate cam and orientation
-            transform.rotation = Quaternion.Euler(0, .085f, 0);
-            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            // Rotate cam and orientation
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+            orientation.localRotation = Quaternion.Euler(0f, yRotation, 0f);
         }
     }
 }
