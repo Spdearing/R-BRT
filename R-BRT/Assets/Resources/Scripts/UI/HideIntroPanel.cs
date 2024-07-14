63c8af358d221aee757df95659586f80dee90505
Spdearing <142638816+Spdearing@users.noreply.github.com>
2024-07-14 15:30:21 -0400

    Main Menu Intro made

---
+++ R-BRT/Assets/Resources/Scripts/UI/HideIntroPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideIntroPanel : MonoBehaviour
+{
+    public void HidePanel()
+    {
+        gameObject.SetActive(false);
+    }
+}
